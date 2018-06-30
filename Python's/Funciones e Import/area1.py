0436171d6bb97179acbdf6ddb2e57e349e6a73bd
Science <4565318+DevScience@users.noreply.github.com>
2018-06-30 16:14:32 -0300

    Ring area
    Ring area

---
+++ Python's/Funciones e Import/area1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 29 20:08:55 2018
+
+@author: JL
+"""
+
+import math
+
+#r = int(input("ingrese el radio: "))
+
+def area(r):
+	return round(math.pi*r**2,2)
+
+#print (area(r))
